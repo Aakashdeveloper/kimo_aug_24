a3fdc477ce8db98ecd30a96647d1f3cd6fffab06
aakashdeveloper <ahanda205@gmail.com>
2024-09-15 21:49:13 +0530

    javascript

---
+++ javascript/datatype.js
@@ -0,0 +1,113 @@
+es5 > Ecma Script 5
+es6 > Ecma Script 6
+
+Datatypes
+String > "hii" 'hii' "10" 'true' `true`
+Number > 33 24234323 242343243434 223.44 .34
+Boolean > true false
+
+//es5
+var a = 10
+var b = "Hiii"
+var c = true
+
+typeof(a)
+'number'
+typeof(b)
+'string'
+typeof(c)
+'boolean'
+var d = "10"
+undefined
+typeof(d)
+'string'
+var e = `true`
+undefined
+typeof(e)
+'string'
+
+var a = 10
+var b = 20
+
+a+b
+30
+a-b
+-10
+b-a
+10
+a*b
+200
+a/b
+0.5
+4%2
+0
+4%3
+1
+
+var a = "Hii"
+var b = "Javascript"
+undefined
+a+b
+'HiiJavascript'
+
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+
+var a = "Hii"
+var b = 10
+
+a+b
+'Hii10'
+b+a
+'10Hii'
+
+string + string = string
+string + number = string
+number + string = string
+number + number = number
+
+"10"+20+30
+"1020"+30
+"102030"
+
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+"10"+20+30-1
+102029
+10+"20"+30-1
+102029
+10+20+"30"-1
+3029
+
+"hii"-1
+NaN
+
+10-1
+9
+"10"-3
+7
+3 * "90"
+270
+"34"/3
+11.333333333333334
+"34" - "9"
+25
+
+
+"34" + "9"
+'349'
+"10a"-1
+NaN
+2 *"8b"
+NaN
