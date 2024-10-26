813ee8a0808c8bbfb21b85ebcfcc4991fe02d309
aakashdeveloper <ahanda205@gmail.com>
2024-10-26 21:35:17 +0530

    nodejs

--- node/zomapi/apidoc.md
+++ node/zomapi/apidoc.md
@@ -1,8 +1,12 @@
 // Page 1
 > List of city
+* http://localhost:9110/location
 > List of Restaurant
+* http://localhost:9110/restaurants
+
 > Rest wrt city
 > list of quick search
+* http://localhost:9110/mealType
 
 
 //Page2
--- node/zomapi/app.js
+++ node/zomapi/app.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 let port = 9110;
+let {dbConnect,
+    getData} = require('./controller/dbController');
 
 
 app.get('/',(req,res) => {
@@ -11,6 +13,38 @@ app.get('/',(req,res) => {
 app.get('/health',(req,res) => {
     res.send('Hii From Health Route')
 });
+
+//get all city
+app.get('/location',async(req,res) => {
+    let query = {};
+    let collection = "location"
+    let output = await getData(collection,query)
+    res.status(200).send(output)
+})
+
+//get all restuarants
+app.get('/restaurants',async(req,res) => {
+    let query = {};
+    let stateId = req.query.stateId
+
+    if(stateId){
+        query = {state_id:Number(stateId)}
+    }else{
+        query = {}
+    }
+
+    let collection = "restaurants";
+    let output = await getData(collection,query);
+    res.status(200).send(output)
+})
+
+//get all mealtype
+app.get('/mealType',async(req,res) => {
+    let query = {};
+    let collection = "mealType";
+    let output = await getData(collection,query);
+    res.status(200).send(output)
+})
 
 app.listen(port,(err) => {
     if(err) throw err;
